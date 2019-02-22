7bc357df8eb5d686339132de81ac6db95ebeff69
patrox17 <47681419+patrox17@users.noreply.github.com>
2019-02-21 21:39:27 -0600

    Add files via upload

---
+++ MHAME1/src/main/java/com/example/MHAME1/Mhame1Application.java
@@ -0,0 +1,13 @@
+package com.example.MHAME1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mhame1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mhame1Application.class, args);
+	}
+
+}
